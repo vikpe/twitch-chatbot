4ee560513dcf3f213de3f3d03c0d4a057869fd00
Viktor Persson <viktor.persson@arcsin.se>
2022-08-10 13:54:44 +0200

    Add test for GetCommands().

--- chatbot_test.go
+++ chatbot_test.go
@@ -1,3 +1,17 @@
 package chatbot_test
 
-// TODO: ...
+import (
+	"testing"
+
+	"github.com/gempir/go-twitch-irc/v3"
+	"github.com/stretchr/testify/assert"
+	chatbot "github.com/vikpe/twitch-chatbot"
+)
+
+func TestChatbot_GetCommands(t *testing.T) {
+	bot := chatbot.NewChatbot("foo", "oauth:bar", "baz", '!')
+	bot.AddCommand("test1", func(cmd chatbot.Command, msg twitch.PrivateMessage) {})
+	bot.AddCommand("test2", func(cmd chatbot.Command, msg twitch.PrivateMessage) {})
+
+	assert.Equal(t, "test1, test2", bot.GetCommands(", "))
+}
